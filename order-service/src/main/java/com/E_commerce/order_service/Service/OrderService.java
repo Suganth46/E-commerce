0b8adcf8fd16295f9ea42723f6126b38dfa12d1a
Suganth46 <suganthsuganth749@gmail.com>
2026-08-20 10:52:05 +0530

    Refactor OrderService to use WebClient.Builder for inventory service calls and update WebClient configuration for load balancing

--- order-service/src/main/java/com/E_commerce/order_service/Service/OrderService.java
+++ order-service/src/main/java/com/E_commerce/order_service/Service/OrderService.java
@@ -24,7 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
     public void placeOrder(OrderRequest orderRequest) {
         Order order=new Order();
         
@@ -42,8 +42,8 @@ public class OrderService {
         .toList();
 
         // Call Inventory Service if the product is availabe in stock
-       InventoryResponse[] inventoryResponsesArray=webClient.get()
-        .uri("http://localhost:8082/api/inventory", UriBuilder -> UriBuilder.queryParam("skuCode", skuCodes).build())
+       InventoryResponse[] inventoryResponsesArray=webClientBuilder.build().get()
+        .uri("http://inventory-service/api/inventory", UriBuilder -> UriBuilder.queryParam("skuCode", skuCodes).build())
         .retrieve()
         .bodyToMono(InventoryResponse[].class)
         .block();
