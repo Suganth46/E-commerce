0b997e6e7683f32353f7efda775a097309075604
Suganth46 <suganthsuganth749@gmail.com>
2026-09-05 15:20:40 +0530

    Add Spring Security configuration and update application properties

---
+++ discovery-server/src/main/java/com/E_commerce/discovery_server/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.E_commerce.discovery_server.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Configuration
+public class SecurityConfig {
+
+    @Value("${eureka.username}")
+    private String username;
+
+    @Value("${eureka.password}")
+    private String password;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails eurekaUser = User
+                .withUsername(username)
+                .password(password)
+                .authorities("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(eurekaUser);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().authenticated()
+            )
+            .httpBasic(httpBasic -> {});
+
+        return http.build();
+    }
+}
